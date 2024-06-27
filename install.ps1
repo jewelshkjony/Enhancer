aa8948c0d84b4c8691cac1aba96db1092af3054c
Jewel Shikder Jony <75406851+jewelshkjony@users.noreply.github.com>
2024-06-27 21:50:28 +0600

    Update install.ps1
    This scripts is only for Windows PowerShell

--- install.ps1
+++ install.ps1
@@ -1,13 +1,7 @@
-# Try 3
-# Base64 encoded URL
-$baseUrl = "aHR0cHM6Ly9naXRodWIuY29tL2pld2Vsc2hram9ueS9FbmhhbmNlci9yZWxlYXNlcy9kb3dubG9hZC8xLjAuMC9FbmhhbmNlci5leGU="
-
-# Decode the base64 string
-$decodedBytes = [System.Convert]::FromBase64String($baseUrl)
-$url = [System.Text.Encoding]::UTF8.GetString($decodedBytes)
-
-# Output the decoded URL
-Write-Output $url
+# Scripts to install Enhancer 1.0.0
+$base = "aHR0cHM6Ly9naXRodWIuY29tL2pld2Vsc2hram9ueS9FbmhhbmNlci9yZWxlYXNlcy9kb3dubG9hZC8xLjAuMC9FbmhhbmNlci5leGU="
+$baseBytes = [System.Convert]::FromBase64String($base)
+$url = [System.Text.Encoding]::UTF8.GetString($baseBytes)
 
 # Define the destination path dynamically using the current user's profile path
 $destination = "$env:LOCALAPPDATA\Enhancer\Enhancer.exe"
@@ -16,14 +10,9 @@ $destinationDir = "$env:LOCALAPPDATA\Enhancer"
 # Create the directory if it doesn't exist
 if (-not (Test-Path -Path $destinationDir)) {
     New-Item -ItemType Directory -Path $destinationDir -Force
-    Write-Output "Created directory: $destinationDir"
-} else {
-    Write-Output "Directory already exists: $destinationDir"
 }
 
-# Download the file
 Invoke-WebRequest -Uri $url -OutFile $destination
-Write-Output "Downloaded Enhancer.exe to: $destination"
 
 # Update PATH for the user
 $User = [EnvironmentVariableTarget]::User
@@ -31,9 +20,6 @@ $Path = [Environment]::GetEnvironmentVariable('Path', $User)
 if (!(";$Path;".ToLower() -like "*;$destinationDir;*".ToLower())) {
     [Environment]::SetEnvironmentVariable('Path', "$Path;$destinationDir", $User)
     $Env:Path += ";$destinationDir"
-    Write-Output "Added $destinationDir to user PATH."
-} else {
-    Write-Output "$destinationDir is already in the user PATH."
 }
 
-Write-Output "Enhancer has been successfully installed and the path has been set."
+Write-Output "Enhancer-1.0.0 has been successfully installed."
