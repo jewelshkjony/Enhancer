e9f067bbde4b7a4a068d94c9e5b2cdb3b6ddeae0
Jewel Shikder Jony <75406851+jewelshkjony@users.noreply.github.com>
2024-07-17 23:00:58 +0600

    Create uninstall.ps1

---
+++ uninstall.ps1
@@ -0,0 +1,20 @@
+# Define the destination path dynamically using the current user's profile path
+$destinationDir = "$env:LOCALAPPDATA\Enhancer"
+
+# Delete the destination directory if it already exists
+if (Test-Path -Path $destinationDir) {
+    Remove-Item -Path $destinationDir -Recurse -Force
+}
+
+# Remove the directory from the user's PATH
+$User = [EnvironmentVariableTarget]::User
+$Path = [Environment]::GetEnvironmentVariable('Path', $User)
+
+# Remove the directory from the PATH only if it exists
+if ($Path -like "*$destinationDir*") {
+    $NewPath = ($Path -split ";") -ne $destinationDir -join ";"
+    [Environment]::SetEnvironmentVariable('Path', $NewPath, $User)
+    $Env:Path = $NewPath
+}
+
+Write-Output "Enhancer-2.0.0 has been successfully uninstalled."
